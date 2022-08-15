095cf9eb45cf909b0174b5d9e21107e19e3e824e
MaRS-208 <manuela.riossosa@gmail.com>
2022-08-14 23:44:22 -0300

    4. Client configuration file (w/ Puppet)

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,6 @@
+# Set up your client SSH configuration file so that you can connect to a server without typing a password
+
+exec { 'ssh_config':
+  path    => '/usr/bin/',
+  command => 'cat 2-ssh_config > ssh_config'
+}
