dd31ab7a71ffde3cc8185267cac27b5799915842
MaRS-208 <manuela.riossosa@gmail.com>
2022-08-22 17:40:00 -0300

    6. Install Nginx web server (w/ Puppet)II

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,6 +1,6 @@
 # Install and configure an Nginx server using Puppet instead of Bash
 
-exec { '3-redirection':
+exec { '1-install_3-redirection':
   path    => '/usr/bin/',
-  command => './3-redirection'
+  command => './1-install_nginx_web_server, ./3-redirection'
 }
