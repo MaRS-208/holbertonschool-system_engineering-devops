6340503022f3b97f06f00fd10c6244a4342a1843
MaRS-208 <manuela.riossosa@gmail.com>
2022-09-08 19:37:12 -0700

    3. Dictionary of list of dictionaries

---
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Using what you did in the task #0, extend your\
+Python script to export data in the JSON format"""
+
+
+if __name__ == "__main__":
+    import json
+    import requests
+    from sys import argv
