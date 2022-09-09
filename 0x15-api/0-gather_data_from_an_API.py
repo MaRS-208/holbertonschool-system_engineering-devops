9033c775f185d6fa08dd6dc5aa97b6507705bf46
MaRS-208 <manuela.riossosa@gmail.com>
2022-09-08 19:28:32 -0700

    0. Gather data from an API III

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -2,7 +2,7 @@
 """Write a Python script that, using this REST API, for a given\
 employee ID, returns information about his/her TODO list progress"""
 
-import requests
-from sys import argv
 
 if __name__ == "__main__":
+    import requests
+    from sys import argv
